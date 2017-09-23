57a3d4dd006af21ef2d4e3fabc322f797c02f43e
pamornkul <pamorn2000@gmail.com>
2017-09-23 15:15:09 +0700

    Add files via upload

---
+++ eclipCpp.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : eclipCpp.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	printf("Hello Wolrd");
+
+	return 0;
+}
